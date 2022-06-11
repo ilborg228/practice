e766bffb58f6b39b16e1617c8903076f753d95fa
Ilia <asd120200@gmail.com>
2022-06-11 21:54:34 +0400

    book controller

--- src/main/java/com/example/practice/controller/BookController.java
+++ src/main/java/com/example/practice/controller/BookController.java
@@ -1,12 +1,32 @@
 package com.example.practice.controller;
 
+import com.example.practice.dto.BookDto;
+import com.example.practice.entity.Book;
 import com.example.practice.service.BookService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
 public class BookController extends BaseController{
 
     private final BookService bookService;//TODO
+
+    @PostMapping("/books")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Book add(@RequestBody BookDto bookDto){
+        return bookService.add(bookDto);
+    }
+
+    @GetMapping("/books/{id}")
+    public BookDto getBookDetails(@PathVariable Long id){
+        return bookService.getById(id);
+    }
+
+    @DeleteMapping("/books/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete(@PathVariable Long id){
+        bookService.delete(id);
+    }
 }
--- src/main/java/com/example/practice/service/BookService.java
+++ src/main/java/com/example/practice/service/BookService.java
@@ -1,9 +1,27 @@
 package com.example.practice.service;
 
+import com.example.practice.dto.BookDto;
+import com.example.practice.entity.Book;
+import com.example.practice.repository.BookRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class BookService extends BaseService{
+
+    private final BookRepository bookRepository;
+
+
+    public Book add(BookDto bookDto) {
+        return null;
+    }
+
+    public BookDto getById(Long id) {
+        return null;
+    }
+
+    public void delete(Long id) {
+
+    }
 }
